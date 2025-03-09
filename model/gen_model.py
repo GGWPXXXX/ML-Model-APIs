9cabcd47488d090612543539bcf5c89951f5bcb6
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2025-03-09 16:25:01 +0700

    Refactor comments for consistency and add model.pkl file

--- model/gen_model.py
+++ model/gen_model.py
@@ -3,16 +3,16 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
-# Load dataset
+# load dataset
 iris = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(
     iris.data, iris.target, test_size=0.2, random_state=42)
 
-# Train model
+# train model
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
-# Save model
+# save model
 with open("model/model.pkl", "wb") as f:
     pickle.dump(model, f)
 
